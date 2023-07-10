edb5ddfb5cb210275ca8cf5faad55671082781d3
KazariEX <1364035137@qq.com>
2023-07-11 03:00:19 +0800

    切蛇优化

--- src/note/arc.ts
+++ src/note/arc.ts
@@ -1,6 +1,5 @@
 import Hold from "./hold.js";
 import TimingGroup from "./timinggroup.js";
-import { ColorType, EasingType } from "../enum.js";
 import { getComplexCurveByEasing } from "../utils/easing.js";
 
 class Arc extends Hold
@@ -10,7 +9,7 @@ class Arc extends Hold
     easing: EasingType;
     y1: number;
     y2: number;
-    color: ColorType;
+    color: number;
     hitsound: string;
     skyline: boolean;
     arctap: Array<number>;
@@ -20,17 +19,15 @@ class Arc extends Hold
         timeEnd = 0,
         x1 = 0,
         x2 = 0,
-        easing = EasingType.b,
+        easing = "b" as EasingType,
         y1 = 0,
         y2 = 0,
-        color = ColorType.blue,
+        color = 0,
         hitsound = "none",
         skyline = false,
         arctap = Array()
-    } = {}) {
-        super();
-        this.time = time;
-        this.timeEnd = timeEnd;
+    }: ArcOptions = {}) {
+        super({ time, timeEnd });
         this.x1 = x1;
         this.x2 = x2;
         this.easing = easing;
@@ -42,10 +39,15 @@ class Arc extends Hold
         this.arctap = arctap;
     }
 
-    at(t: number): Point
+    at(t: number, { cx, cy }: {
+        cx?: EasingFunction,
+        cy?: EasingFunction
+    } = {}): Point
     {
         const percent = (t - this.time) / (this.timeEnd - this.time);
-        const [cx, cy] = getComplexCurveByEasing(this.easing);
+        const [_cx, _cy] = getComplexCurveByEasing(this.easing);
+        cx ??= _cx;
+        cy ??= _cy;
 
         return {
             x: this.x1 + cx(percent) * (this.x2 - this.x1),
@@ -53,65 +55,116 @@ class Arc extends Hold
         };
     }
 
-    clone(): Arc
+    clone({
+        time = this.time,
+        timeEnd = this.timeEnd,
+        x1 = this.x1,
+        x2 = this.x2,
+        easing = this.easing,
+        y1 = this.y1,
+        y2 = this.y2,
+        color = this.color,
+        hitsound = this.hitsound,
+        skyline = this.skyline,
+        arctap = Array.from(this.arctap)
+    }: ArcOptions = this): Arc
     {
         return new Arc({
-            time: this.time,
-            timeEnd: this.timeEnd,
-            x1: this.x1,
-            x2: this.x2,
-            easing: this.easing,
-            y1: this.y1,
-            y2: this.y2,
-            color: this.color,
-            hitsound: this.hitsound,
-            skyline: this.skyline,
-            arctap: Array.from(this.arctap)
+            time, timeEnd, x1, x2, easing, y1, y2, color, hitsound, skyline, arctap
         });
     }
 
-    cut(count: number, { cx, cy }: {
-        cx: EasingFunction,
-        cy: EasingFunction
-    }): TimingGroup
+    cut(count: number, { start = this.time, end = this.timeEnd, ender = false, cx, cy }: {
+        start?: number,
+        end?: number,
+        ender?: boolean,
+        cx?: EasingFunction,
+        cy?: EasingFunction
+    } = {}): TimingGroup
     {
         const tg = new TimingGroup();
-        if (count === 1) {
-            tg.push(this.clone());
-        }
-        else if (count > 1) {
-            const st = (this.timeEnd - this.time) / count;
-            const [_cx, _cy] = getComplexCurveByEasing(this.easing);
-            cx ??= _cx;
-            cy ??= _cy;
+        if (count >= 1) {
+            // 模板音弧
+            const sample = this.clone();
+            sample.easing = "s";
+            sample.arctap = [];
 
-            const last = {
-                x: this.x1,
-                y: this.y1
-            };
-            const next = {
-                x: 0,
-                y: 0
-            };
-            for (let i = 0; i < count; i++) {
-                const percent = (i + 1) / count;
-                next.x = this.x1 + cx(percent) * (this.x2 - this.x1);
-                next.y = this.y1 + cy(percent) * (this.y2 - this.y1);
-                tg.push(new Arc({
-                    time: this.time + i * st,
-                    timeEnd: this.time + (i + 1) * st,
-                    x1: last.x,
-                    x2: next.x,
-                    easing: EasingType.s,
-                    y1: last.y,
-                    y2: next.y,
-                    color: this.color,
-                    hitsound: this.hitsound,
-                    skyline: this.skyline,
-                    arctap: []
-                }));
-                last.x = next.x;
-                last.y = next.y;
+            // 时间范围限定
+            start = Math.max(this.time, Math.min(this.timeEnd, start));
+            end = Math.max(this.time, Math.min(this.timeEnd, end));
+            if (start > end) [start, end] = [end, start];
+            sample.time = start;
+            sample.timeEnd = end;
+
+            // 起点&终点坐标
+            const p1 = this.at(start, { cx, cy });
+            const p2 = this.at(end, { cx, cy });
+            sample.x1 = p1.x;
+            sample.y1 = p1.y;
+            sample.x2 = p2.x;
+            sample.y2 = p2.y;
+
+            if (count === 1) {
+                tg.push(sample);
+            }
+            else {
+                const st = (sample.timeEnd - sample.time) / count;
+                const [_cx, _cy] = getComplexCurveByEasing(this.easing);
+                cx ??= _cx;
+                cy ??= _cy;
+
+                const per_s = (sample.time - this.time) / this.duration;
+                const per_d = (sample.timeEnd - sample.time) / this.duration;
+
+                const last = {
+                    x: sample.x1,
+                    y: sample.y1
+                };
+                const next = {
+                    x: 0,
+                    y: 0
+                };
+                for (let i = 0; i < count; i++) {
+                    const percent = per_s + per_d * (i + 1) / count;
+                    next.x = this.x1 + cx(percent) * (this.x2 - this.x1);
+                    next.y = this.y1 + cy(percent) * (this.y2 - this.y1);
+                    tg.push(new Arc({
+                        time: sample.time + i * st,
+                        timeEnd: sample.time + (i + 1) * st,
+                        x1: last.x,
+                        x2: next.x,
+                        easing: "s",
+                        y1: last.y,
+                        y2: next.y,
+                        color: sample.color,
+                        hitsound: sample.hitsound,
+                        skyline: sample.skyline
+                    }));
+                    last.x = next.x;
+                    last.y = next.y;
+                }
+            }
+
+            // 两端音弧
+            if (ender) {
+                if (start > this.time) {
+                    tg.unshift(this.clone({
+                        timeEnd: start,
+                        x2: p1.x,
+                        easing: "s",
+                        y2: p1.y,
+                        arctap: []
+                    }));
+                }
+                if (end < this.timeEnd) {
+                    tg.push(this.clone({
+                        time: end,
+                        x1: p2.x,
+                        easing: "s",
+                        y1: p2.y,
+                        arctap: []
+                    }));
+                }
             }
         }
         return tg;
