b478c4282193273fd8fa0e21422d4ed1eaeee7f5
KazariEX <1364035137@qq.com>
2024-07-07 01:42:41 +0800

    refactor(parser): 重用两个解析方法的代码逻辑

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -5,66 +5,39 @@ import { parser } from "./parser";
 import { locationFromToken } from "./utils";
 import { visitor } from "./visitor";
 
-export function parseAff(text: string): Aff {
-    const errors: AFFError[] = [];
-    const lexingResult = lexer.tokenize(text);
+export const parseAff = createParser<Aff>("head", "aff");
+export const parseNote = createParser<Note | TimingGroup>("body", "item");
 
-    errors.push(...lexingResult.errors.map((e) => ({
-        message: e.message,
-        location: {
-            startOffset: e.offset,
-            endOffset: e.offset + e.length,
-            startLine: e.line,
-            endLine: e.line,
-            startColumn: e.column,
-            endColumn: e.column! + e.length
+function createParser<T>(lexerMode: "head" | "body", parserRule: "aff" | "item") {
+    return function(input: string) {
+        const errors: AFFError[] = [];
+        const lexingResult = lexer.tokenize(input, lexerMode);
+
+        errors.push(...lexingResult.errors.map((e) => ({
+            message: e.message,
+            location: {
+                startOffset: e.offset,
+                endOffset: e.offset + e.length,
+                startLine: e.line,
+                endLine: e.line,
+                startColumn: e.column,
+                endColumn: e.column! + e.length
+            }
+        })));
+
+        parser.input = lexingResult.tokens;
+        const parsingResult = parser[parserRule]();
+
+        errors.push(...parser.errors.map((e) => ({
+            message: e.message,
+            location: locationFromToken(e.token)
+        })));
+
+        const res = visitor.visit(parsingResult, errors) as T;
+
+        if (errors.length > 0) {
+            throw new Error(`[Line:${errors[0].location.startLine}] ${errors[0].message}`);
         }
-    })));
-
-    parser.input = lexingResult.tokens;
-    const parsingResult = parser.aff();
-
-    errors.push(...parser.errors.map((e) => ({
-        message: e.message,
-        location: locationFromToken(e.token)
-    })));
-
-    const aff = visitor.visit(parsingResult, errors);
-
-    if (errors.length > 0) {
-        throw new Error(`[Line:${errors[0].location.startLine}] ${errors[0].message}`);
-    }
-    return aff;
-}
-
-export function parseNote<T extends Note | TimingGroup>(text: string): T {
-    const errors: AFFError[] = [];
-    const lexingResult = lexer.tokenize(text, "body");
-
-    errors.push(...lexingResult.errors.map((e) => ({
-        message: e.message,
-        location: {
-            startOffset: e.offset,
-            endOffset: e.offset + e.length,
-            startLine: e.line,
-            endLine: e.line,
-            startColumn: e.column,
-            endColumn: e.column! + e.length
-        }
-    })));
-
-    parser.input = lexingResult.tokens;
-    const parsingResult = parser.item();
-
-    errors.push(...parser.errors.map((e) => ({
-        message: e.message,
-        location: locationFromToken(e.token)
-    })));
-
-    const note = visitor.visit(parsingResult, errors);
-
-    if (errors.length > 0) {
-        throw new Error(`[Line:${errors[0].location.startLine}] ${errors[0].message}`);
-    }
-    return note;
+        return res;
+    };
 }
